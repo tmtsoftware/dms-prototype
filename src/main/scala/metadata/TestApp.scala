abed154e5705324b0eb93f710441f1b198bbd110
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-10-15 12:01:13 +0530

    simplified TestApp

--- src/main/scala/metadata/TestApp.scala
+++ src/main/scala/metadata/TestApp.scala
@@ -4,54 +4,47 @@ import java.sql.Timestamp
 
 import akka.actor.typed.{ActorSystem, SpawnProtocol}
 import csw.database.DatabaseServiceFactory
-import csw.database.scaladsl.JooqExtentions.{RichQuery, RichResultQuery}
+import csw.database.scaladsl.JooqExtentions.RichQuery
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.ActorSystemFactory
 import csw.location.client.scaladsl.HttpLocationServiceFactory
-import csw.params.events.{EventName, SystemEvent}
+import csw.params.core.formats.ParamCodecs.paramEncExistential
+import csw.params.core.generics.KeyType.StringKey
+import csw.params.events.{Event, EventName, SystemEvent}
 import csw.prefix.models.Prefix
 import csw.prefix.models.Subsystem.ESW
+import io.bullet.borer.Json
 import org.jooq.DSLContext
-import org.jooq.impl.DSL.jsonbObject
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
 object TestApp extends App {
-
   implicit val system: ActorSystem[SpawnProtocol.Command] = ActorSystemFactory.remote(SpawnProtocol())
+  private val locationService: LocationService            = HttpLocationServiceFactory.makeLocalClient
+  private val context: DSLContext                         = Await.result(new DatabaseServiceFactory(system).makeDsl(locationService, "mydb"), 10.seconds)
 
-  import system.executionContext
+  private val payload: String = ("0" * 1024)
+  private val event: SystemEvent =
+    SystemEvent(Prefix(ESW, "filter"), EventName("wheel5")).add(StringKey.make("key").set(payload))
 
-  private val locationService: LocationService = HttpLocationServiceFactory.makeLocalClient
-
-  private val context: DSLContext = Await.result(new DatabaseServiceFactory(system).makeDsl(locationService, "mydb"), 10.seconds)
-
-  private val event: SystemEvent = SystemEvent(Prefix(ESW, "filter"), EventName("wheel"))
-
-  println(event.eventTime)
-
-//  private val nObject = new JSONObject(new util.HashMap(1))
-
-//  private val records = context.resultQuery("select count(*) from event_snapshots;").fetchAsyncScala[Int]
-//  println(Await.result(records, 10.seconds) + "=========================")
-
-  private val eventualInteger = context
+  val startTime = System.currentTimeMillis()
+  val eventualInteger = context
     .query(
-      "INSERT INTO event_snapshots values (?,?,?,?,?,?,?)",
-      "expId1",
-      "obs_event_name1",
-      "event_source1",
-      "event_name",
-      "event_id",
-      new Timestamp(0),
-      jsonbObject()
+      s"""
+      INSERT INTO event_snapshots values ${eventSeq("expId7000", "startExposure", event)}
+      """
     )
     .executeAsyncScala()
-
-  println(Await.result(eventualInteger, 10.seconds))
-  println("-------------------------------")
+  println(Await.result(eventualInteger, 5.seconds))
+  println("-------------------------------" + (System.currentTimeMillis() - startTime))
 
   system.terminate()
   Await.result(system.whenTerminated, 10.seconds)
+
+  private def eventSeq(expId: String, obsEventName: String, event: Event) =
+    s"""('$expId','$obsEventName','${event.source.toString()}','${event.eventName.name}',
+        '${event.eventId.id}','${Timestamp.from(event.eventTime.value)}',
+        '${Json.encode(event.paramSet).toUtf8String}'::Json)
+    """
 }
